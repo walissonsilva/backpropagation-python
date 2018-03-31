74f46538eac81c91a6a2f6c7d0bea10f4df4aea3
Walisson Silva <walissonsilva10@gmail.com>
2018-03-31 11:58:46 -0300

    Primeiro Commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Back-Propagation em Python
+
+- O algoritmo do Backpropagation implementado na linguagem Python 2.7.
+
+- O algoritmo foi desenvolvido para a aproximação de funções. Foram implementadas três métodos com três funções distintas: `load_first_function`, `load_second_function` e `load_third_function`.
+
+![Resultado com a primeira função](imagens/resultado-backpropagation.png)
+![Erro na aproximação da primeira função](imagens/MSE-backpropagation.png)
---
+++ backpropagation.py
@@ -0,0 +1,101 @@
+import matplotlib.pyplot as plt  # Para plotar graficos
+import numpy as np  # Array do Python
+from sklearn import datasets  # Biblioteca sklearn para carregar os dados
+from math  import sqrt, pi
+
+class Backpropagation(object):
+    def __init__(self, eta=0.01, epoch_max=20000, Ni=2, Nh=5, Ns=1):
+        self.eta = eta
+        self.epoch_max = epoch_max
+        self.Ni = Ni
+        self.Nh = Nh
+        self.Ns = Ns
+        self.Wini = 0.5
+
+    def load_first_function(self):
+        x = np.arange(-6, 6, 0.2)
+        self.N = x.shape[0]
+        xmax = np.max(x)
+
+        self.X_train = x / xmax
+        self.d = 1 / (1 + np.exp(-1 * x))*(np.cos(x) - np.sin(x))
+    
+    def load_second_function(self):
+        x = np.arange(-6, 6, 0.24)
+        self.N = x.shape[0]
+        xmax = np.max(x)
+
+        self.X_train = x / xmax
+        self.d = np.sin(x) / x
+    
+    def load_third_function(self):
+        x = np.arange(-5, 15, 0.25)
+        self.N = x.shape[0]
+        xmax = np.max(x)
+
+        self.X_train = x# / xmax
+        self.d = np.exp(-(x - 5)**2 / (2 * 2.71**2)) / (2.71 * sqrt(2 * pi))
+    
+    def train(self):
+        self.Wji = np.random.rand(self.Nh, self.Ni) * self.Wini
+        self.Wkj = np.random.rand(self.Ns, self.Nh + 1) * self.Wini
+
+        MSE = np.zeros(self.epoch_max)
+        plt.ion()
+
+        for epoca in xrange(self.epoch_max):
+            deltaWkj = deltaWji = 0
+            z = np.zeros(self.N)
+            E = np.zeros(self.N)
+
+            for i in xrange(self.N):
+                xi = np.array([-1, self.X_train[i]]).reshape(1, -1)
+                netj = np.dot(self.Wji, xi.T)
+                yj = 1 / (1 + np.exp(-netj.T))
+                yj_pol = np.insert(yj[0], 0, -1).reshape(1, -1)
+                z[i] = np.dot(self.Wkj, yj_pol.T)[0][0]
+
+                e = self.d[i] - z[i]
+                etae = - self.eta * e
+                deltaWkj -= np.dot(etae, yj_pol)
+                deltaWji -= np.dot(etae * (self.Wkj[:,1:] * yj * (1 - yj)).T, xi)
+
+                E[i] = 0.5 * e**2
+            
+            self.Wkj += deltaWkj
+            self.Wji += deltaWji
+
+            MSE[epoca] = np.sum(E) / self.N
+
+            if (epoca % 200 == 0 or epoca == self.epoch_max - 1):
+                if (epoca != 0):
+                    plt.cla()
+                    plt.clf()
+                
+                self.plot(z, epoca)
+
+        plt.ioff()
+        plt.figure(1)
+        plt.title('Mean Square Error (MSE)')
+        plt.xlabel('Training Epochs')
+        plt.ylabel('MSE')
+        plt.plot(np.arange(0, MSE.size), MSE)
+        plt.show()
+
+    def plot(self, saida, epoca):
+        plt.figure(0)
+        y, = plt.plot(self.X_train, saida, label="y")
+        d, = plt.plot(self.X_train, self.d, label="d")
+        plt.legend([y, d], ['Output of Network Neural', 'Desired Value'])
+        plt.xlabel('x')
+        plt.ylabel('f(x)')
+        plt.text(np.min(self.X_train) - np.max(self.X_train) * 0.17, np.min(self.d) - np.max(self.d) * 0.17, 'Progress: ' + str(round(float(epoca) / self.epoch_max * 100, 2)) + '%')
+        plt.axis([np.min(self.X_train) - np.max(self.X_train) * 0.2, np.max(self.X_train) * 1.2, np.min(self.d) - np.max(self.d) * 0.2, np.max(self.d) * 1.2])
+        plt.show()
+        plt.pause(1e-10)
+
+back = Backpropagation()
+
+back.load_first_function()
+#back.load_third_function()
+back.train()
